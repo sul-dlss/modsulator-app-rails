9f395ac54000323fac93727062392d71cbb141e0
Justin Coyne <jcoyne@justincoyne.com>
2018-03-15 09:51:17 -0500

    Remove unused framework components

--- app/controllers/normalizer_controller.rb
+++ app/controllers/normalizer_controller.rb
@@ -1,7 +1,4 @@
 class NormalizerController < ApplicationController
-  # This is an API, so CSRF token verification doesn't apply
-  skip_before_action :verify_authenticity_token
-
   def create
     input_file = File.open(File.new(params[:file].tempfile))
     xml = input_file.read
--- app/helpers/spreadsheet_helper.rb
+++
@@ -1,2 +0,0 @@
-module SpreadsheetHelper
-end
