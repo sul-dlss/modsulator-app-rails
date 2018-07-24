a32235569fe5e1c1e7f6599ed95927b8407c6dc9
Justin Coyne <jcoyne@justincoyne.com>
2018-07-24 13:26:49 -0700

    Link the config files

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,7 +24,7 @@ set :deploy_to, "/opt/app/modsulator/modsulator-app-rails"
 # set :pty, true
 
 # Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+set :linked_files, %w(config/secrets.yml config/honeybadger.yml)
 
 # Default value for linked_dirs is []
 set :linked_dirs, ['log', 'tmp']
