4eec1785bdd67b2efdded1f3f9ec9febd4e87322
Justin Coyne <jcoyne@justincoyne.com>
2018-07-24 10:30:47 -0700

    Add capistrano

---
+++ config/deploy/prod.rb
@@ -0,0 +1,6 @@
+server 'modsulator-app-rails-prod.stanford.edu', user: 'modsulator', roles: %w{web db app}
+
+set :rails_env, 'production'
+set :bundle_without, %w(test development deploy).join(' ')
+
+Capistrano::OneTimeKey.generate_one_time_key!
