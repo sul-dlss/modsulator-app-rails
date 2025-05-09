3e13f8849bee1b8f11e7d66810e7334edceb5cbb
Michael J. Giarlo <mjgiarlo@stanford.edu>
2025-05-09 13:45:25 -0700

    Remove explicit one-time key setup

--- config/deploy/qa.rb
+++ config/deploy/qa.rb
@@ -4,5 +4,3 @@ server 'modsulator-app-qa.stanford.edu', user: 'modsulator', roles: %w[web db ap
 
 set :rails_env, 'production'
 set :bundle_without, %w[test development deploy].join(' ')
-
-Capistrano::OneTimeKey.generate_one_time_key!
